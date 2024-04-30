75ba05d09878f53e0fea2afec414cb743f3b7fd2
Pheona Ohakwe <pohakwe@gmail.com>
2024-04-29 21:14:55 -0500

    Backend_Day1

---
+++ Day 1 hw.py
@@ -0,0 +1,46 @@
+#QUESTION 1:
+#Task1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Task2: Your Mood Today
+if "happy":
+    print("That's great to hear!")
+if "sad":
+    print("I hope your day gets better!")
+
+#QUESTION 2:
+pi_value = 3.14
+print(pi_value)
+user_age = 25
+print(user_age)
+user_location = "New York"
+print(user_location)
+MAX_LIMIT = 1000
+print(MAX_LIMIT)
+
+#QUESTION 3:
+
+#Task1
+bread,peanutbutter,jelly= 3.75, 5.00, 4.25
+grocery_haul= bread+peanutbutter+jelly
+print(grocery_haul)
+
+#Task2
+#fixed yearly interest rate balance calculation
+
+
+initial_amount = 10000
+interest_rate =.07
+simple_interest = float(initial_amount) * interest_rate
+time = 1
+answer = simple_interest * time
+end_of_the_year_balance= answer+initial_amount
+print(end_of_the_year_balance)
+                            
+
+
